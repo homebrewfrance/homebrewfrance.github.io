8dc77a6dc3dfb0b90aacc9b23737452f94e04c1f
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-08-01 16:15:12 +0200

    Update cookies.js

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,14 +1,23 @@
         const cookieBox = document.getElementById('cookie-box');
         const acceptBtn = document.getElementById('accept-btn');
+        const declineBtn = document.getElementById('decline-btn');
 
         const hasAcceptedCookies = localStorage.getItem('cookiesAccepted');
+        const hasDeclinedCookies = localStorage.getItem('cookiesDeclined');
 
-        if (!hasAcceptedCookies) {
+        if (!hasAcceptedCookies && !hasDeclinedCookies) {
             cookieBox.style.display = 'block';
         }
 
         acceptBtn.addEventListener('click', () => {
             localStorage.setItem('cookiesAccepted', 'true');
+                
+            cookieBox.style.display = 'none';
+        });
+
+        declineBtn.addEventListener('click', () => {
+
+            localStorage.setItem('cookiesDeclined', 'true');
 
             cookieBox.style.display = 'none';
-        })
+        });
