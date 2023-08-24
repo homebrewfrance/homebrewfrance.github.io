b38860cbf1d07067eac7848b78d6cfc406438cfd
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-08-24 16:40:24 +0200

    Update ns-serial-checker.js

--- scripts/ns-serial-checker.js
+++ scripts/ns-serial-checker.js
@@ -1,76 +1,63 @@
-      const form = document.querySelector('form');
-      form.addEventListener('submit', (e) => {
-          e.preventDefault();
-          const numberInput = document.querySelector('input[name="numberInput"]').value;
-          const xawSelector = document.querySelector('select[name="xawSelector"]').value;
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const numberInput = document.querySelector('input[name="numberInput"]').value;
+    const xawSelector = document.querySelector('select[name="xawSelector"]').value;
 
-          if (xawSelector === "XAW1") {
-            if (numberInput >= 0000000000 && numberInput <= 0074000000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0074000000 && numberInput <= 0120000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0120000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-          }
-		  if (xawSelector === "XAW4") {
-            if (numberInput >= 0000000000 && numberInput <= 0011000000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0011000000 && numberInput <= 0012000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0012000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-		  }
-		  if (xawSelector === "XAW7") {
-            if (numberInput >= 0000000000 && numberInput <= 0017800000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0017800000 && numberInput <= 0030000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0030000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-		  }
-		  if (xawSelector === "XAJ1") {
-            if (numberInput >= 0000000000 && numberInput <= 0020000000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0020000000 && numberInput <= 0030000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0030000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-		  }
-		  if (xawSelector === "XAJ4") {
-            if (numberInput >= 0000000000 && numberInput <= 0046000000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0046000000 && numberInput <= 0060000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0060000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-		  }
-		  if (xawSelector === "XAJ7") {
-            if (numberInput >= 0000000000 && numberInput <= 0040000000) {
-              alert("Votre console n'est pas patchée");
-            } else if (numberInput >= 0040000000 && numberInput <= 0050000000) {
-              alert("Votre console est potentiellement patchée");
-			} else if (numberInput >= 0050000000) {
-			  alert("Votre console est patchée")
-            } else {
-              alert("Numéro de série invalide");
-            }
-		  }
-          else if (xawSelector === "") {
-            alert("Not supported");
-          }
-      });
+    if (numberInput.length !== 11) {
+        alert("Numéro de série invalide, il doit comporter exactement 11 caractères.");
+        return;
+    }
+
+    if (xawSelector === "XAW") {
+        if (numberInput >= 10000000000 && numberInput <= 10074000000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 10074000000 && numberInput <= 10120000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 10120000000) {
+            alert("Votre console est patchée");
+        } else if (numberInput >= 40000000000 && numberInput <= 40011000000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 40011000000 && numberInput <= 40012000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 40012000000) {
+            alert("Votre console est patchée");
+        } else if (numberInput >= 70000000000 && numberInput <= 70017800000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 70017800000 && numberInput <= 70030000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 70030000000) {
+            alert("Votre console est patchée");
+        } else {
+            alert("Numéro de série invalide");
+        }
+    } else if (xawSelector === "XAJ") {
+        if (numberInput >= 10000000000 && numberInput <= 10020000000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 10020000000 && numberInput <= 10030000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 10030000000) {
+            alert("Votre console est patchée");
+        } else if (numberInput >= 40000000000 && numberInput <= 40046000000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 40046000000 && numberInput <= 40060000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 40060000000) {
+            alert("Votre console est patchée");
+        } else if (numberInput >= 70000000000 && numberInput <= 70040000000) {
+            alert("Votre console n'est pas patchée");
+        } else if (numberInput > 70040000000 && numberInput <= 70050000000) {
+            alert("Votre console est potentiellement patchée");
+        } else if (numberInput > 70050000000) {
+            alert("Votre console est patchée");
+        } else {
+            alert("Numéro de série invalide");
+        }
+    } else if (["XKJ", "XJW", "XKW", "XWW"].includes(xawSelector)) {
+        if (numberInput >= 10000000000) {
+            alert("Votre console est patchée");
+        }
+    } else {
+        alert("Numéro de série invalide");
+    }
+});
