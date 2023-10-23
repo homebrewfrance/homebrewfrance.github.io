694f169e80535a0a50c9a0d956d5e31c75119654
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-10-23 18:10:41 +0200

    Update cookies.js

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,4 +1,41 @@
-        const cookieBox = document.getElementById('cookie-box');
+document.addEventListener("DOMContentLoaded", function() {
+    var cookieBox = document.getElementById("cookie-box");
+    var acceptButton = document.getElementById("accept-btn");
+    var declineButton = document.getElementById("decline-btn");
+  
+    var userDecision = document.cookie.replace(/(?:(?:^|.*;\s*)userCookieDecision\s*=\s*([^;]*).*$)|^.*$/, "$1");
+  
+    if (userDecision === "accepted") {
+      cookieBox.style.display = "none";
+    } else if (userDecision === "declined") {
+      var gtmScript = document.querySelector('script[src="https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124"]');
+      if (gtmScript) {
+        gtmScript.parentNode.removeChild(gtmScript);
+      }
+      cookieBox.style.display = "none";
+    } else {
+      cookieBox.style.display = "block";
+    }
+  
+    acceptButton.addEventListener("click", function() {
+      document.cookie = "userCookieDecision=accepted; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
+      cookieBox.style.display = "none";
+    });
+  
+    declineButton.addEventListener("click", function() {
+      document.cookie = "userCookieDecision=declined; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
+      cookieBox.style.display = "none";
+      var gtmScript = document.querySelector('script[src="https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124"]');
+      if (gtmScript) {
+        gtmScript.parentNode.removeChild(gtmScript);
+      }
+    });
+  });
+
+
+
+
+/*const cookieBox = document.getElementById('cookie-box');
         const acceptBtn = document.getElementById('accept-btn');
         const declineBtn = document.getElementById('decline-btn');
 
@@ -20,4 +57,4 @@
             localStorage.setItem('cookiesDeclined', 'true');
 
             cookieBox.style.display = 'none';
-        });
+        });*/
