d6443e6d8caaefc53a328bf3e1687c7d24953208
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-11-18 13:57:29 +0100

    Create console-ver.js

---
+++ scripts/console-ver.js
@@ -0,0 +1,14 @@
+switchFWVer = "17.0.0"
+switchFWName = "1700"
+
+var switchVer = document.getElementById("switchVer");
+var switchVer1 = document.getElementById("switchVer1");
+var switchVer2 = document.getElementById("switchVer2");
+var switchFirmName = document.getElementById("switchFirmName");
+var switchFirmName1 = document.getElementById("switchFirmName1");
+
+switchVer.innerHTML = switchFWVer;
+switchVer1.innerHTML = switchFWVer;
+switchVer2.innerHTML = switchFWVer;
+switchFirmName.innerHTML = switchFWName;
+switchFirmName1.innerHTML = switchFWName;
