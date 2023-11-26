22420b15d3c6f06da48419fc10d8614e8e948752
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-11-26 16:48:59 +0100

    Update console-ver.js

--- scripts/console-ver.js
+++ scripts/console-ver.js
@@ -4,11 +4,18 @@ switchFWName = "1700"
 var switchVer = document.getElementById("switchVer");
 var switchVer1 = document.getElementById("switchVer1");
 var switchVer2 = document.getElementById("switchVer2");
+var switchVer3 = document.getElementById("switchVer3");
+var switchVer4 = document.getElementById("switchVer4");
 var switchFirmName = document.getElementById("switchFirmName");
 var switchFirmName1 = document.getElementById("switchFirmName1");
+var switchFirmName2 = document.getElementById("switchFirmName2");
 
 switchVer.innerHTML = switchFWVer;
 switchVer1.innerHTML = switchFWVer;
 switchVer2.innerHTML = switchFWVer;
+switchVer3.innerHTML = switchFWVer;
+switchVer4.innerHTML = switchFWVer;
 switchFirmName.innerHTML = switchFWName;
 switchFirmName1.innerHTML = switchFWName;
+switchFirmName2.innerHTML = switchFWName;
+
