50952d141b1bfaf1c800cc55e2ae7daad4210f7e
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-12-16 13:54:40 +0100

    Update cookies.js
    Nouvelle gestion des cookies. En corrélation avec la génération dynamique des éléments statiques des pages.
    
    Utilisable uniquement avec les nouvelles versions du site.

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,60 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cookieBox = document.getElementById("cookie-box");
-    var acceptButton = document.getElementById("accept-btn");
-    var declineButton = document.getElementById("decline-btn");
-  
-    var userDecision = document.cookie.replace(/(?:(?:^|.*;\s*)userCookieDecision\s*=\s*([^;]*).*$)|^.*$/, "$1");
-  
-    if (userDecision === "accepted") {
-      cookieBox.style.display = "none";
-    } else if (userDecision === "declined") {
-      var gtmScript = document.querySelector('script[src="https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124"]');
-      if (gtmScript) {
-        gtmScript.parentNode.removeChild(gtmScript);
-      }
-      cookieBox.style.display = "none";
-    } else {
-      cookieBox.style.display = "block";
-    }
-  
-    acceptButton.addEventListener("click", function() {
-      document.cookie = "userCookieDecision=accepted; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
-      cookieBox.style.display = "none";
-    });
-  
-    declineButton.addEventListener("click", function() {
-      document.cookie = "userCookieDecision=declined; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
-      cookieBox.style.display = "none";
-      var gtmScript = document.querySelector('script[src="https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124"]');
-      if (gtmScript) {
-        gtmScript.parentNode.removeChild(gtmScript);
-      }
-    });
-  });
+const userDecision = localStorage.getItem('cookieConsent');
 
+function loadGoogleAnalytics() {
+	const script = document.createElement('script');
+	script.async = true;
+	script.src = 'https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124';
+	document.head.appendChild(script);
 
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-68CMDJM124');
+}
 
+if (userDecision === 'accepted') {
+	loadGoogleAnalytics();
+}
 
-/*const cookieBox = document.getElementById('cookie-box');
-        const acceptBtn = document.getElementById('accept-btn');
-        const declineBtn = document.getElementById('decline-btn');
+document.getElementById('accept-btn').addEventListener('click', function() {
+	localStorage.setItem('cookieConsent', 'accepted');
+	loadGoogleAnalytics();
+});
 
-        const hasAcceptedCookies = localStorage.getItem('cookiesAccepted');
-        const hasDeclinedCookies = localStorage.getItem('cookiesDeclined');
-
-        if (!hasAcceptedCookies && !hasDeclinedCookies) {
-            cookieBox.style.display = 'block';
-        }
-
-        acceptBtn.addEventListener('click', () => {
-            localStorage.setItem('cookiesAccepted', 'true');
-                
-            cookieBox.style.display = 'none';
-        });
-
-        declineBtn.addEventListener('click', () => {
-
-            localStorage.setItem('cookiesDeclined', 'true');
-
-            cookieBox.style.display = 'none';
-        });*/
+document.getElementById('decline-btn').addEventListener('click', function() {
+	localStorage.setItem('cookieConsent', 'declined');
+});
