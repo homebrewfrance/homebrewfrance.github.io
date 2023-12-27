06812f2b6990062aa472d9b589ae06524a97c20d
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-12-27 16:18:47 +0100

    Update navbar__f.js

--- scripts/navbar__f.js
+++ scripts/navbar__f.js
@@ -10,7 +10,8 @@ var htmlLang = document.getElementsByTagName('html')[0].getAttribute('lang');
 var footerTag = document.getElementsByTagName('footer');
 
 document.addEventListener('DOMContentLoaded', function () {
-    pageGeneration();
+	pageGeneration();
+	loadGoogleTagManager();
 });
 
 var navContent = '<nav class="navbar navbar-expand-lg"><a class="navbar-brand" href="https://homebrewfrance.github.io/"><img src="https://raw.githubusercontent.com/homebrewfrance/homebrewfrance.github.io/main/IMAGES/favicon.png" width="45" height="45"></a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav mr-auto"><li class="nav-item mr-1 ml-1"><a id="accueil-nav" class="nav-link" href="https://homebrewfrance.github.io/">Accueil</a></li><li class="nav-item mr-1 ml-1"><a id="packs-nav" class="nav-link" href="https://homebrewfrance.github.io/telechargements">Téléchargements</a></li><li class="nav-item mr-1 ml-1 dropdown"><a id="docs-nav" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Docs</a><div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item" href="https://homebrewfrance.github.io/docs/switch">Nintendo Switch</a><a class="dropdown-item" href="https://homebrewfrance.github.io/docs/dsi">Nintendo DSi</a><a class="dropdown-item disabled" href="https://homebrewfrance.github.io/docs/3ds">Nintendo 3DS</a><a class="dropdown-item" href="https://homebrewfrance.github.io/docs/ps2">PlayStation 2</a></div></li><li class="nav-item mr-1 ml-1 dropdown"><a id="checker-nav" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Multi-Checker</a><div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item" href="https://homebrewfrance.github.io/multi-checker/switch">Nintendo Switch</a><a class="dropdown-item" href="https://homebrewfrance.github.io/multi-checker/ps3">PlayStation 3</a></div></li><li class="nav-item mr-1 ml-1"><a id="boutiqueNav" class="nav-link" href="https://homebrewfrance.github.io/boutique">Boutique</a></li><li class="nav-item mr-1 ml-1"><a id="about-nav" class="nav-link" href="https://homebrewfrance.github.io/a-propos">A propos</a></li></ul></div></nav>';
@@ -48,6 +49,19 @@ function pageGeneration() {
 	/*activeLinks();*/
 }
 
+function loadGoogleTagManager() {
+	var gtagScript = document.createElement('script');
+	gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-68CMDJM124';
+	gtagScript.async = true;
+	
+	var head = document.head || document.getElementsByTagName('head')[0];
+	head.appendChild(gtagScript);
+	
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'G-68CMDJM124');
+}
 // UNUSED WITH NEW UI
 /*function activeLinks() {
 	var accueilNav = document.getElementById('accueil-nav');
