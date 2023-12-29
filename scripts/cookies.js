7cc2cfb8cef8dc1ddcbf1c33883d57dc90c0f7b8
Dhalian <67284307+Dhalian@users.noreply.github.com>
2023-12-29 14:49:01 +0100

    Fixed cookie box. Added href link in redirection if it doesn't work.

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,14 +1,20 @@
 /* CONTRIBUTE TO THIS WEBSITE ON OUR GITHUB https://github.com/homebrewfrance/homebrewfrance.github.io */
-/* LAST-EDITED : 27/12/2023 by Dhalian */
+/* LAST-EDITED : 29/12/2023 by Dhalian */
 
 function checkAccepted() {
   return localStorage.getItem('cookiesAccepted') === 'true';
 }
 
 function hideBanner() {
-  const cookieBanner = document.getElementById('cookieBanner');
+  var cookieBoxGen = document.getElementById("cookieBoxGen");
+  var cookieContent = '<div id="cookie-banner-container"><button class="cookie-banner" id="cookieBanner"><div class="cookie-container"><p>En poursuivant, vous acceptez l’utilisation de cookies pour améliorer l’expérience utilisateur.<div class="cookie-banner-button" onclick="acceptCookies()">J’accepte</div></p></div></button></div>'
+
   if (checkAccepted()) {
-    cookieBanner.style.display = 'none';
+    console.log("Cookies already accepted.")
+    cookieBoxGen.style.display = "none";
+  }
+  else {
+    cookieBoxGen.innerHTML = cookieContent;
   }
 }
 
--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,5 +1,5 @@
 /* CONTRIBUTE TO THIS WEBSITE ON OUR GITHUB https://github.com/homebrewfrance/homebrewfrance.github.io */
-/* LAST-EDITED : 27/12/2023 by Dhalian */
+/* LAST-EDITED : 29/12/2023 by Dhalian */
 
 DiscordLink = 'https://discord.gg/le-homebrew-france-883623179979984896';
 NHLink = 'https://discord.gg/C29hYvh';
@@ -11,18 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function redirection() {
+	var redirectLink = document.getElementById('redirect-link');
 	var pageSource = document.referrer;
 	if (pageSource.includes('https://homebrewfrance.github.io/discord')) {
 		window.location.href = DiscordLink;
+		redirectLink.href = DiscordLink;
 	}
 	else if (pageSource.includes('https://homebrewfrance.github.io/nh')) {
 		window.location.href = NHLink;
+		redirectLink.href = NHLink;
 	}
 	else if (pageSource.includes('https://homebrewfrance.github.io/disboard')) {
 		window.location.href = DisboardLink;
+		redirectLink.href = DisboardLink;
 	}
 	else if (pageSource.includes('https://homebrewfrance.github.io/devenir-expert')) {
 		window.location.href = FormulaireHelperLink;
+		redirectLink.href = FormulaireHelperLink;
 	}
 }
 	
