7be800fd92ffd45d9daab53dca20c60e85defb41
Dhalian <67284307+Dhalian@users.noreply.github.com>
2024-01-21 20:31:50 +0100

    Change manual links

--- scripts/manuel.js
+++ scripts/manuel.js
@@ -1,12 +1,12 @@
 /* CONTRIBUTE TO THIS WEBSITE ON OUR GITHUB https://github.com/homebrewfrance/homebrewfrance.github.io */
-/* LAST-EDITED : 27/12/2023 by Dhalian */
+/* LAST-EDITED : 21/01/2024 by Dhalian */
 
 // MANUELS DISPONIBLES :
 // 3DS - DEMAT 
-Man3DSDemat = 'https://hbf-files.github.io/download/HBFDOCE1R13DS.pdf';
+Man3DSDemat = 'https://hbf-files.github.io/download/HBFDOCE1R23DS.pdf';
 
 // 3DS - IMRPIMABLE
-Man3DSPrintable = 'https://hbf-files.github.io/download/HBFDOCE1R13DS-PRNTABLE.pdf';
+Man3DSPrintable = 'https://hbf-files.github.io/download/HBFDOCE1R23DS-PRNTABLE.pdf';
 
 
 
