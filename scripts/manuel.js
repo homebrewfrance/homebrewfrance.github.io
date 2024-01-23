d469b3a747a3e506f5c49d420aadb0fd2741835f
Dhalian <67284307+Dhalian@users.noreply.github.com>
2024-01-23 21:02:13 +0100

    Update manuel.js
    Fixed links + removed testing console log

--- scripts/manuel.js
+++ scripts/manuel.js
@@ -1,12 +1,12 @@
 /* CONTRIBUTE TO THIS WEBSITE ON OUR GITHUB https://github.com/homebrewfrance/homebrewfrance.github.io */
-/* LAST-EDITED : 21/01/2024 by Dhalian */
+/* LAST-EDITED : 23/01/2024 by Dhalian */
 
 // MANUELS DISPONIBLES :
 // 3DS - DEMAT 
-Man3DSDemat = 'https://hbf-files.github.io/download/HBFDOCE1R23DS.pdf';
+Man3DSDemat = 'https://homebrewfrance.github.io/download/HBFDOCE1R23DS.pdf';
 
 // 3DS - IMRPIMABLE
-Man3DSPrintable = 'https://hbf-files.github.io/download/HBFDOCE1R23DS-PRNTABLE.pdf';
+Man3DSPrintable = 'https://homebrewfrance.github.io/download/HBFDOCE1R23DS-PRNTABLE.pdf';
 
 
 
@@ -16,11 +16,9 @@ function traitement() {
         alert("Vous devez choisir une version !")
     }
     else if (ThreedsManVersion == 'demat') {
-        console.log("demat choisi" + ThreedsManVersion.value);
         window.location.href = Man3DSDemat;
     }
     else if (ThreedsManVersion == 'printable') {
-        console.log("pribntable choisi" + ThreedsManVersion.value);
         window.location.href = Man3DSPrintable;
     }
 }
