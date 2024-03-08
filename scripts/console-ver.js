bb81acd2a54d009b95fcecc6c6686b4e9187b131
Dhalian <67284307+Dhalian@users.noreply.github.com>
2024-03-08 14:11:46 +0100

    Auto fetch for NX firmware. Auto fill for downloads and shop. Other minor changes

--- scripts/console-ver.js
+++ scripts/console-ver.js
@@ -1,17 +1,30 @@
-var switchFWVer = "17.0.1";
-var switchFWName = "1701";
+/* CONTRIBUTE TO THIS WEBSITE ON OUR GITHUB https://github.com/homebrewfrance/homebrewfrance.github.io */
+/* LAST-EDITED : 08/03/2024 by Dhalian */
 
 var switchVer = document.getElementsByClassName("switchVer");
 var switchFirmName = document.getElementsByClassName("switchFirmName");
 
-document.addEventListener("DOMContentLoaded", function() {
-  for (var i = 0; i < switchVer.length; i++) {
-    switchVer[i].innerHTML = switchFWVer;
-    console.log("switchVer chargé");
-  }
+$(document).ready(function() {
+  var repoOwner = "THZoria";
+  var repoName = "NX_Firmware";
+  var apiUrl = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/releases/latest";
 
-  for (var j = 0; j < switchFirmName.length; j++) {
-    switchFirmName[j].innerHTML = switchFWName;
-    console.log("switchFirmName chargé");
-  }
+  $.get(apiUrl, function(data) {
+      var latestRelease = data.tag_name;
+      for (var i = 0; i < switchVer.length; i++) {
+        switchVer[i].innerHTML = latestRelease.slice(0, -1);
+      }
+      let switchFWName = latestRelease.substring(0, 2) + latestRelease.substring(3, 4) + latestRelease.substring(5);
+      for (var i = 0; i < switchFirmName.length; i++) {
+        switchVer[i].innerHTML = switchFWName.slice(0, -1);
+      }
+      console.log(switchFWName.slice(0, -1));
+  }).fail(function() {
+    for (var i = 0; i < switchVer.length; i++) {
+      switchVer[i].innerHTML = "Echec de la récupération du firmware actuel";
+    }
+    for (var i = 0; i < switchFirmName.length; i++) {
+      switchVer[i].innerHTML = "Echec de la récupération du firmware actuel";
+    }
+  });
 });
